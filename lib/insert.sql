f25551fae3d71c4b46131ef677144d07a3c906e2
Andrew Dziabo <jameszabo@yahoo.com>
2022-09-21 11:52:54 -0400

    commit tree 57d85e4b83e4cdcb29f284e907429bfba721ed13

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 13, "M", "brown", "chill", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 6, "M", "white", "spunky", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 8, "F", "brown", "calm", 1);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 15, "F", "black", "intense", 1);
+INSERT INTO bears VALUES (5,"Melissa", 10, "F", "brown", "aggresive", 1);
+INSERT INTO bears VALUES (6,"Grinch", 1, "M", "brown", "bad", 1);
+INSERT INTO bears VALUES (7,"Wendy", 3, "F", "brown", "hyper", 1);
+INSERT INTO bears VALUES (8,null, 52, "M", "brown", "chill", 1);
